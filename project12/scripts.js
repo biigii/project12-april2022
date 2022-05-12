bb7b1c8ca49c532b2779de89ddf8a8df63d310df
behshad <82580731+biigii@users.noreply.github.com>
2022-05-12 21:02:38 +1200

    javascript added

---
+++ project12/scripts.js
@@ -0,0 +1,52 @@
+// alert('hello world');
+
+// var const let
+// let username
+// let num1
+// let num2
+// let sum
+
+// // string
+// const country = 'Japan'
+// // country = 'USA';
+
+// username = 'John'
+// username = 1000
+// username = true
+
+// // number
+// num1 = 100
+// num2 = 200
+
+// num2 = 500
+// sum = num1 + num2
+
+// // boolean
+// const isNight = true
+
+// const emptyArray = []
+// const countries = ['Japan', 'USA', 'Korea']
+// const numbers = [100, 200, 300]
+
+// console.log(sum)
+// // console.log(username)
+// console.log(country)
+// console.log(isNight)
+
+// const word1 = 'hello'
+// const word2 = 'world'
+
+// console.log(word1 + " " + word2)
+
+
+document.querySelector("h1").style.color = 'red';
+document.querySelector('body').style.backgroundColor = '#ff0';
+document.querySelector('.text').style.fontSize = '50px';
+document.querySelector('.text').style.textDecoration = 'underline';
+
+document.querySelector('#image').style.border = '20px solid #f77'
+document.querySelector('#image').style.borderRadius = '50%';
+document.getElementById('heading').style.border = '10px solid #000';
+
+
+document.querySelector('[data-image]').style.border = '10px solid #080';
